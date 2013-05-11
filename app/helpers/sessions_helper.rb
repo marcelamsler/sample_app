c74fe4c88068eeeb80162d4fdbc4c1dbd6d7aad2
Marcel Amsler <marcel.amsler@gmail.com>
2013-05-11 12:21:31 +0200

    added signin and signout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,5 +3,27 @@ module SessionsHelper
 	def sign_in(user)
 		cookies.permanent[:remember_token] = user.remember_token
 		self.current_user = user
+	end
+
+	def current_user=(user)
+		@current_user = user
 	end	
+
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])	
+	end
+	
+	def signed_in?
+		!current_user.nil?
+	end	
+
+	def sign_out
+		session[:user_id] = nil
+    	cookies.delete(:remember_token)
+	end	
+
+
+
+
+
 end
